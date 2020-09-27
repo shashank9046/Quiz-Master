1be3e472d80e5ac36fb11e7f50b398bf5907011c
RohanBhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2020-09-27 18:35:33 +0530

    splash screen issue resolved

--- app/src/main/java/com/example/quizmaster/MainActivity.java
+++ app/src/main/java/com/example/quizmaster/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.quizmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int Splash_time_out=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashActivity = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(splashActivity);
+                finish();
+
+            }
+        },Splash_time_out);
     }
 }
