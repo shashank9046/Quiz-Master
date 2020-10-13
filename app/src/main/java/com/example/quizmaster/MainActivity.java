5ead72f6d604651754669b7e4ab23c7467a57bee
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2020-10-13 13:09:19 +0530

    modifiying main activity
    modifying main activity to fetch data from the database
    
    Co-Authored-By: Shivani Chauhan <48038121+Shivani-781@users.noreply.github.com>
    Co-Authored-By: Vanshika Mahle <50958987+VanshikaMahle@users.noreply.github.com>
    Co-Authored-By: Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
    Co-Authored-By: Shashank  Pandey <skppandey2017@gmail.com>

--- app/src/main/java/com/example/quizmaster/MainActivity.java
+++ app/src/main/java/com/example/quizmaster/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.quizmaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static int Splash_time_out=3000;
+    public static List<String> catList = new ArrayList<>();
+    private FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firestore=FirebaseFirestore.getInstance();
+        loadData(); //loads data before the category activity starts from the firebase
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -21,5 +35,31 @@ public class MainActivity extends AppCompatActivity {
 
             }
         },Splash_time_out);
+    }
+
+    private void loadData(){
+        catList.clear();
+        firestore.collection("QUIZ").document("Categories").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot doc = task.getResult();
+                    if(doc.exists()){
+                        long count = (long)doc.get("COUNT");
+                        for(int i=1;i<=count;i++){
+                            String catName = doc.getString("CAT"+String.valueOf(i));
+                            catList.add(catName);
+                        }
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,"No category exists",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+                else{
+                    Toast.makeText(MainActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
